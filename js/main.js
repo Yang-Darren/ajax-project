e04426049c6e029b69aef964866dbe9435cd2f6e
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-06-11 17:47:34 +0000

    added placeholder function for search to reset when enter key is pressed.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
-var pokemon = new XMLHttpRequest();
-var $pokemonList = document.querySelector('#pokemon-list');
+var $search = document.querySelector('#search');
+$search.addEventListener('keydown', search);
 
-pokemon.open('GET', 'https://api.pokemontcg.io/v2/cards');
-pokemon.responseType = 'json';
-pokemon.addEventListener('load', function () {
-  var firstNumber = Math.floor(Math.random() * 250);
-  var $firstPokemon = document.createElement('li');
-  var $firstSet = document.createElement('p');
-  var $firstImg = document.createElement('img');
-  var $firstPrice = document.createElement('p');
-  $firstPokemon.textContent = pokemon.response.data[firstNumber].name;
-  $firstSet.textContent = pokemon.response.data[firstNumber].set.name;
-  $firstImg.setAttribute('src', pokemon.response.data[firstNumber].images.small);
-  $firstPrice.textContent = pokemon.response.data[firstNumber].tcgplayer.prices.market;
-  $pokemonList.appendChild($firstPokemon);
-  $pokemonList.appendChild($firstSet);
-  $pokemonList.appendChild($firstImg);
-  $pokemonList.appendChild($firstPrice);
-});
-
-pokemon.send();
+function search() {
+  if (event.key === 'Enter') {
+    $search.value = '';
+  }
+}
