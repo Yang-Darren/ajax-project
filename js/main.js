6f3e64f0e285798cc03946e410250bcd71a574fc
Darren Yang <90783131+Yang-Darren@users.noreply.github.com>
2022-06-15 04:26:34 +0000

    Added function to hide home page when searching for pokemon.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 var $search = document.querySelector('#search');
+var $homePage = document.querySelector('#home-page');
 $search.addEventListener('keydown', search);
 
 function search() {
   if (event.key === 'Enter') {
     $search.value = '';
+    $homePage.className = 'hidden';
   }
 }
+
+var pokemon = new XMLHttpRequest();
+var $pokemonList = document.querySelector('#pokemon-list');
+
+pokemon.open('GET', 'https://api.pokemontcg.io/v2/cards');
+pokemon.responseType = 'json';
+pokemon.addEventListener('load', function () {
+  var firstNumber = Math.floor(Math.random() * 250);
+  var $firstPokemon = document.createElement('li');
+  var $firstSet = document.createElement('p');
+  var $firstImg = document.createElement('img');
+  var $firstPrice = document.createElement('p');
+  $firstPokemon.textContent = pokemon.response.data[firstNumber].name;
+  $firstSet.textContent = pokemon.response.data[firstNumber].set.name;
+  $firstImg.setAttribute('src', pokemon.response.data[firstNumber].images.small);
+  $firstPrice.textContent = pokemon.response.data[firstNumber].tcgplayer.prices.market;
+  $pokemonList.appendChild($firstPokemon);
+  $pokemonList.appendChild($firstSet);
+  $pokemonList.appendChild($firstImg);
+  $pokemonList.appendChild($firstPrice);
+});
+
+pokemon.send();
